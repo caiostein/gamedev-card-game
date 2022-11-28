2607c1528954bd2fb5b84365b9872aed2217ef7e
caiostein <caio.kaki98@gmail.com>
2022-11-28 01:43:29 -0300

    Implemented Player name input and persistency

---
+++ Assets/Scripts/PlayerNameHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerNameHandler : MonoBehaviour
+{
+    public TMP_Text playerName;
+
+    public void SetPlayerNameInput(TMP_Text playerName)
+    {
+        ScoreManager.Instance.playerName = playerName;
+        Debug.Log(playerName.text);
+    }
+}
