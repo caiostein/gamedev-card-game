704358a9379989f736439ca7c1418c3c938fb4d4
caiostein <caio.kaki98@gmail.com>
2022-12-23 00:00:47 -0300

    Added initial dialog sequence

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour
+{
+    public TextMeshProUGUI dialogText;
+    public int currentLineNumber;
+    
+    //Screen Elements
+    public GameObject nextButton;
+    public GameObject nameInputField;
+    public GameObject submitNameButton;
+    public GameObject skipButton;
+    public GameObject playButton;
+
+
+
+    private void Start()
+    {
+        currentLineNumber = 1;
+        dialogText.text = Enum.dialogLines[currentLineNumber];
+    }
+
+    public void IncreaseCurrentLineNumber()
+    {
+        currentLineNumber++;
+    }
+
+
+    public void GetNextDialogLine()
+    {
+        if(currentLineNumber < Enum.dialogLines.Count)
+        {
+            IncreaseCurrentLineNumber();
+
+            string lineToShow = Enum.dialogLines[currentLineNumber];
+
+            switch (currentLineNumber)
+            {
+                case 5:
+                    nextButton.SetActive(false);
+                    nameInputField.SetActive(true);
+                    submitNameButton.SetActive(true);
+                    break;
+                case 6:
+                    lineToShow = lineToShow.Replace("[PlayerName]", ScoreManager.Instance.playerName.text);
+                    break;
+                case 7:
+                    skipButton.SetActive(true);
+                    break;
+                case 10:
+                    nextButton.SetActive(false);
+                    skipButton.SetActive(false);
+                    playButton.SetActive(true);
+                    break;
+            }
+
+            dialogText.text = lineToShow;
+        }
+    }
+}
--- Assets/Scripts/Enum.cs
+++ Assets/Scripts/Enum.cs
@@ -73,4 +73,18 @@ public class Enum
 		{3, "Bom Gamer." },
 		{4, "Deus Gamer." },
 	};
+
+	public static readonly IDictionary<int, string> dialogLines = new Dictionary<int, string>()
+	{
+		{1, "- ... -" },
+		{2, "- Você chega à Feira de Games da sua região -" },
+		{3, "- Você está ansioso para participar de sua primeira Game Jam e mostrar ao mundo a sua capacidade de criar jogos incríveis -" },
+		{4, "- Logo você encontra um funcionário que te recebe para a competição -" },
+		{5, "Olá! Que bom que você chegou! Me diga: como você se chama?" },
+		{6, "Olá [PlayerName]! Bem vindo ao game jam. Logo logo você já vai estar criando o seu jogo. Mas antes vamos falar sobre as regras da competição." },
+		{7, "Caso deseje pular essa explicação, clique em 'Pular'." },
+		{8, "Você deve criar seu jogo a partir de certos elementos de jogo que estarão relacionados com 4 categorias distintas. Essas categorias são os pilares da tétrade de Schell." },
+		{9, "Para isso você fará um brainstorming a partir de cartas que te apresentarão diferentes elementos de jogos. Você deve escolher 4 cartas para cada categoria." },
+		{10, "Mas fique ligado, as cartas tem mais ou menos afinidade com uma categoria da tétrade. Você será avaliado de acordo com as cartas que você escolher, portanto escolha com sabedoria." },
+	};
 }
